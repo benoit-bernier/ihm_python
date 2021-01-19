ff6f91daaa2e7e97f1676c587f80eefee381f1e5
benoit-bernier <b.benoit22100@gmail.com>
2021-01-19 11:49:38 +0100

    ajout joli piou

--- piou_p5.py
+++ piou_p5.py
@@ -4,5 +4,17 @@ from piou import Piou
 
 class Piou_P5 (Piou):
     def display(self):
-        fill(Color(218, 247, 166))
-        circle(self.pos_x, self.pos_y, self.radius)
+        #fill(Color(218, 247, 166))
+        #circle(self.pos_x, self.pos_y, self.radius)
+        with push_matrix():
+            translate(self.pos_x, self.pos_y)
+            scale(0.3)
+            fill(0)
+            ellipse(50,20, 40, 40)
+            rect(45,15,25,20)
+            triangle(70,18,70,35,80,25)
+            arc((50, 35), 40, 80, 0, PI + QUARTER_PI, mode="PIE")
+            triangle(50,75,0,75,45,2)
+            fill(255)
+            ellipse(55,15,5,5)
+            fill(0)
